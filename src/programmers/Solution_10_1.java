3b793486c7da379beb00c2f20e70c124a2d1ae17
nami <dnska6657@naver.com>
2023-06-18 13:48:47 +0900

    [programmers]  짝수와 홀수

---
+++ src/programmers/Solution_10_1.java
@@ -0,0 +1,18 @@
+package programmers;
+
+class Solution_10_1 {
+    public String solution(int num) {
+        String answer = "";
+
+        if(num % 2 == 0){
+            answer = "Even";
+        } else {
+            answer = "Odd";
+        }
+        return answer;
+    }
+}
+
+/*
+짝수와 홀수
+ */
