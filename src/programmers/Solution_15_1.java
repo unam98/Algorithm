a021845efc1657c9ccd63e4902cc9681de5ffea6
nami <dnska6657@naver.com>
2023-06-23 23:10:05 +0900

    [programmers] 문자열을 정수로 바꾸기

---
+++ src/programmers/Solution_15_1.java
@@ -0,0 +1,12 @@
+package programmers;
+
+class Solution_15_1 {
+    public int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+}
+
+/*
+문자열을 정수로 바꾸기
+ */
