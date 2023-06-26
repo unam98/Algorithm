1199a8a7b1fc7225769f7feb8a60e5e61b4504fe
nami <dnska6657@naver.com>
2023-06-26 16:17:00 +0900

    [programmers] 서울에서 김서방 찾기

---
+++ src/programmers/Solution_17_1.java
@@ -0,0 +1,19 @@
+package programmers;
+
+class Solution_17_1 {
+    public String solution(String[] seoul) {
+        String answer = "";
+
+        for(int i=0; i<seoul.length; i++) {
+            if(seoul[i].equals("Kim")) {
+                answer = "김서방은 " + i + "에 있다";
+            }
+        }
+
+        return answer;
+    }
+}
+
+/*
+서울에서 김서방 찾기
+ */
