4635e0c0b2211ef18b9ccaa2ab37d67c68f63421
nami <dnska6657@naver.com>
2023-07-11 16:42:33 +0900

    [programmers] 편지

---
+++ src/programmers/Solution_22_1.java
@@ -0,0 +1,10 @@
+package programmers;
+
+class Solution_22_1 {
+    public int solution(String message) {
+        int answer = 0;
+
+        answer = message.length() * 2;
+        return answer;
+    }
+}
